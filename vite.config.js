16f2a84d512019319379f6ed729d94ed69a3cb00
sheeraz <114077246+sheeraz143@users.noreply.github.com>
2024-10-22 18:32:06 +0530

    style error on server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      external: [
+        'public/css/bootstrap.min.css',
+        'public/css/font-awesome.min.css',
+        'public/css/animate.css',
+        'public/css/owl.carousel.css',
+        'public/css/venobox.css',
+        'public/css/styles.css',
+        'public/css/style.css',
+        'public/js/jquery.min.js',
+        'public/js/bootstrap.min.js',
+        'public/js/wow.min.js',
+        'public/js/jquery.backTop.min.js',
+        'public/js/waypoints.min.js',
+        'public/js/waypoints-sticky.min.js',
+        'public/js/owl.carousel.min.js',
+        'public/js/jquery.stellar.min.js',
+        'public/js/jquery.counterup.min.js',
+        'public/js/venobox.min.js',
+        'public/js/custom-scripts.js',
+      ],
+    },
+  },
+});
